18d0a37d46fa8eda5a8571d9db8bdcff14a92edc
TheENGR <TheENGR42@gmail.com>
2021-06-15 09:59:06 -0500

    no errors on send whn in chrome search area

--- src/getElement.js
+++ src/getElement.js
@@ -1,7 +1,7 @@
 console.log('LOADED IN EMBEDDED SCRIPT TO GET RIGHT CLICK EVENTS')
 
 document.addEventListener('contextmenu', e => {
-	//chrome.runtime.sendMessage("afffkcmpebnikjnoagiiofainbpffnch", e);
-	console.log(e)
+	chrome.runtime.sendMessage("afffkcmpebnikjnoagiiofainbpffnch", e);
+	//console.log(e)
 });
 
