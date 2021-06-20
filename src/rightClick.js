f4a569a77e4c7e36e04def4edd8c5b11747cef6a
TheENGR <TheENGR42@gmail.com>
2021-06-20 15:03:48 -0500

    Setup of reporting structure

--- src/rightClick.js
+++ src/rightClick.js
@@ -1,39 +1,33 @@
-// Add right click elements when installed
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.contextMenus.create({
-		title: 'Report HTML Element',
-		type: 'normal',
-		id: 'RightClickReport',
-		contexts: ['all']
-	});
-	
-	chrome.contextMenus.create({
-		title: 'Report Video Element',
-		type: 'normal',
-		id: 'VideoReport',
-		contexts: ['all']
-	});
-	
-	chrome.contextMenus.create({
-		title: 'Report Audio Element',
-		type: 'normal',
-		id: 'AudioReport',
-		contexts: ['all']
-	});
+// Register the Plugin when installed
+chrome.runtime.onInstalled.addListener(() => {
+	// TODO: GET INSTANCE ID FROM LAMBDA
+	chrome.storage.local.set({ instanceID: "xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx" });
+	//chrome.browserAction.setIcon({
+	//	path: {
+	//		"128": 'images/128_CCC_GREEN.png'
+	//	}
+	//})
+	mapMenus();
+});
+
+// Load available infractions when a window is opened
+chrome.windows.onCreated.addListener(async () => {
+	await mapMenus();
 });
 
 //https://developer.chrome.com/docs/extensions/mv3/migrating_to_service_workers/#state
 // Listen for when we are clicked 
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
 	//handle context menu actions
+	//await chrome.browserAction.setIcon('images/128_CCC_BLUE.png')
 	console.log('CLICKED SOMETHING')
 	console.log(info)
 	console.log(tab)
 	console.log('LOAD')
-	chrome.storage.local.get(["htmlElement"], ({ htmlElement }) => {
-		console.log(htmlElement)
-		// Need to send all of this to the Lambdas
-	});
+	const htmlElement = await chrome.storage.local.get(["htmlElement"]);
+	console.log(htmlElement)
+	// TODO: SEND THIS TO THE INFRACTION LOGGING API
+	//await chrome.browserAction.setIcon('images/128_CCC_GREEN.png')
 })
 
 // Listen for the context script to tell us they right clicked on something
@@ -41,3 +35,25 @@ chrome.runtime.onMessage.addListener((message) => {
 	// Gotta store the clicking action
 	chrome.storage.local.set({ htmlElement: message });
 });
+
+const mapMenus = async () => {
+	await chrome.contextMenus.removeAll();
+	
+	// TODO: GET INFRACTIONS FROM LAMBDA
+	const availableInfractions = [{
+		title: "Report Ad with Audio",
+		id:  "AdWithAudio"
+	},{
+		title: "Report Paywall in middle of page",
+		id:  "PaywallInPage"
+	}]
+	
+	availableInfractions.forEach(infraction => {
+		chrome.contextMenus.create({
+			title: infraction.title,
+			type: 'normal',
+			id: infraction.id,
+			contexts: ['all']
+		});
+	})
+}
