bf93e5ecbd2cbdd71c60ca84536e985745754b7e
TheENGR <50523916+TheENGR@users.noreply.github.com>
2021-06-21 19:46:12 -0500

    Right Click (#10)

---
+++ src/main.js
@@ -0,0 +1,12 @@
+//try {
+//  importScripts('requestinterceptor.js');
+//} catch (error) {
+//  console.log(error);
+//}
+
+// Load each part like this 
+try {
+  importScripts('rightClick.js');
+} catch (error) {
+  console.log(error);
+}
