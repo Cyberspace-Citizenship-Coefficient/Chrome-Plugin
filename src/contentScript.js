946e14f750300f88f86c4512ceb1c696942722a2
TheENGR <50523916+TheENGR@users.noreply.github.com>
2021-07-26 20:22:50 -0500

    Update contentScript.js

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,4 +1,3 @@
-// Listen for right clicks then send data on what was clicked
 document.addEventListener('contextmenu', e => {
 	// Send messages to ourself
 	chrome.runtime.sendMessage("afffkcmpebnikjnoagiiofainbpffnch", {
@@ -8,7 +7,7 @@ document.addEventListener('contextmenu', e => {
 				ID: x.id, // HTML ID
 				localName: x.localName // This is a HTML type for some reason
 			}
-		}), // rebuild the array of how to find the element
+		}) ,// rebuild the array of how to find the element
 		action: "Right Click"
 	});
 });
